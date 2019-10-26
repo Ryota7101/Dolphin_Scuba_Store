484deaf951867ab00e193c4459a0485a5638f335
Ryota Suzuki <ryotasuzuki.com@gmail.com>
2019-10-26 10:41:18 +0000

    comment delete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,11 @@
 class User < ApplicationRecord
   has_one :cart #ユーザーとカートを関連つけるために追加
   has_many :orders
-  #belongs_to :order_product 
-  #hoge
   
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
   has_secure_password
   
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
-  
-  
-  
   
 end
